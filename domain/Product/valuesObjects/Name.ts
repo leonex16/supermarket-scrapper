3339fdaa4eade831257f3d217bbb881c9d96feba
Claudio <clandres1985@gmail.com>
2022-02-27 00:49:43 -0300

    :sparkles: feat: Added new validation

--- domain/Product/valuesObjects/Name.ts
+++ domain/Product/valuesObjects/Name.ts
@@ -1,4 +1,5 @@
 import { StrTooShort } from '@Common/exceptions/StrTooShort'
+import { StrTooLarge } from '@Common/exceptions/StrTooLarge'
 
 export class Name {
   private _value: string;
@@ -13,8 +14,10 @@ export class Name {
 
   static create ( name: string ) {
     let returnedValue = null;
+
     try {
       if ( name.length < 2 ) throw new StrTooShort();
+      if ( name.length > 25 ) throw new StrTooLarge();
 
       returnedValue = new Name( name );
 
