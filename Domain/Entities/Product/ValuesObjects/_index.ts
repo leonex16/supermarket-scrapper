24a520fe424b5a5a7f175729bd7f559358d0cf31
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-03-01 20:38:20 -0300

    :triangular_flag_on_post: build: Add index inside value objects of product entity

---
+++ Domain/Entities/Product/ValuesObjects/_index.ts
@@ -0,0 +1,13 @@
+import { Name } from './Name'
+import { Description } from './Description'
+import { Detail } from './Detail';
+import { Image } from './Image';
+import { Source } from './Source';
+
+export {
+  Name,
+  Description,
+  Detail,
+  Image,
+  Source
+}
