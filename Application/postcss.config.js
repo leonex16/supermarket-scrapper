a0c82acd4e082f2a47778ffd86711153f5e9d2dd
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-03-12 21:12:51 -0300

    :wrench: config: Add config file for postcss and stylelint. Update tsconfig file

---
+++ Application/postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: {
+    'autoprefixer': true,
+    'postcss-import': true,
+    'stylelint': true,
+    'cssnano': false,
+    'precss': true,
+  }
+}
---
+++ Application/stylelint.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "extends": [
+    "stylelint-config-standard",
+    "stylelint-config-prettier"
+  ],
+  "rules": {
+    "at-rule-no-unknown": [
+      true,
+      {
+        "ignoreAtRules": [
+          "tailwind",
+          "apply",
+          "variants",
+          "responsive",
+          "screen"
+        ]
+      }
+    ]
+  }
+}
