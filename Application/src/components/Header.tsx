befd928456efd4fab9d1cc8ff697f6c5333acfda
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2022-03-14 08:34:13 -0300

    :sparkles: feat: Add header component

---
+++ Application/src/components/Header.tsx
@@ -0,0 +1,9 @@
+
+export const Header = () => (
+  <header className="header-app">
+    <section className="header-app__supermarket-color header-app__supermarket-color--tottus" />
+    <section className="header-app__supermarket-color header-app__supermarket-color--lider" />
+    <section className="header-app__supermarket-color header-app__supermarket-color--jumbo" />
+    <section className="header-app__supermarket-color header-app__supermarket-color--santa-isabel" />
+  </header>
+)
