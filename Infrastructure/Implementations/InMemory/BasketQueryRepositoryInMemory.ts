63a7f7c251d13452a2b4671ca39e7525e5a5da26
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-03-19 05:51:45 -0300

    :triangular_flag_on_post: build: Implement repositories at Infrastructure

---
+++ Infrastructure/Implementations/InMemory/BasketQueryRepositoryInMemory.ts
@@ -0,0 +1,21 @@
+import { Basket } from '../../../Domain/Entities/Basket/Basket';
+import { BasketQueryRepository } from '../../../Domain/Entities/Basket/Respositories';
+
+// import { Basket } from '@Domain/Basket/Basket';
+// import { BasketQueryRepository } from '@Domain/Basket/Respositories';
+
+import basketsRaw from './data/baskets.json';
+
+export class BasketQueryRepositoryInMemory implements BasketQueryRepository {
+  private readonly _basketInMemory: any[] = basketsRaw;
+
+  getBasket( basketId: string ): Promise<Basket | null > {
+    const foundBasket = this._basketInMemory.find( basket => basket.id === basketId );
+
+    return Promise.resolve( 
+      foundBasket !== undefined 
+        ? Basket.create( foundBasket )
+        : null 
+    );
+  }
+}
