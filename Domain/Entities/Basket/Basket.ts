3730d7386971544007f5168b28202872b2a4fb36
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-03-19 05:50:38 -0300

    :triangular_flag_on_post: build: Update basket entity

--- Domain/Entities/Basket/Basket.ts
+++ Domain/Entities/Basket/Basket.ts
@@ -1,24 +1,26 @@
-import { Entity } from "@Common/Entity";
-// import { UUID } from '@Common/Gateways/UUID';
-import { findByIdAndRemove } from "@Common/functions/findByIdAndRemove";
+import { Entity } from '../../Common/Entity';
+import { Product } from '../Product/Product';
 
-import { Product } from "@Entities/Product/Product";
+// import { Entity } from '@Common/Entity';
+// import { Product } from '@Entities/Product/Product';
 
+interface BasketProps {
+  id: string;
+  items: Product[]
+}
 export class Basket extends Entity {
-  private _items: Product[] = []
+  public items: Product[];
 
-  // constructor( uuid: UUID ) {
-  //   super( uuid );
-  // }
-
-  public add( product: Product ) {
-    this._items.push( product );
+  private constructor( { id, items }: BasketProps ) {
+    super( id );
+    this.items = items;
   }
 
-  public remove( productId: string ) {
-    this._items = findByIdAndRemove( this._items, productId );
-
-    return this
+  get id(): string {
+    return this._id;
   }
 
+  static create( { id, items }: BasketProps ): Basket {
+    return new Basket( { id, items } );
+  }
 }
