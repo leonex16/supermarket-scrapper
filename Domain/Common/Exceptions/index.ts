03f32f6e1be74e0f28794f61427f4bc711d596fa
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-03-19 05:44:51 -0300

    :triangular_flag_on_post: build: Add domain excepcions

---
+++ Domain/Common/Exceptions/index.ts
@@ -0,0 +1,63 @@
+import { CustomErrorProps } from '../Types';
+// import { CustomErrorProps } from '@Common/Types';
+
+export class EmptyValueException extends Error {
+  
+  constructor ( customErrorProps?: CustomErrorProps ) {
+    const message = 'Valor vacío';
+
+    super(
+      customErrorProps?.customMessage ?? message
+      // customErrorProps?.opts
+    );
+    this.name = 'EmptyValue';
+  }
+}
+
+export class RequiredParamException extends Error {
+  constructor ( customErrorProps?: CustomErrorProps ) {
+    const message = 'Valor requerido';
+
+    super(
+      customErrorProps?.customMessage ?? message
+      // customErrorProps?.opts
+    );
+    this.name = 'RequiredParam';
+  }
+}
+
+export class StrTooLargeException extends Error {
+  constructor ( customErrorProps?: CustomErrorProps ) {
+    const message = 'Longitud de cadena demasiado larga';
+
+    super(
+      customErrorProps?.customMessage ?? message
+      // customErrorProps?.opts
+    );
+    this.name = 'StrTooLarge';
+  }
+}
+
+export class StrTooShortException extends Error {
+  constructor ( customErrorProps?: CustomErrorProps ) {
+    const message = 'Longitud de cadena demasiado corta';
+
+    super(
+      customErrorProps?.customMessage ?? message
+      // customErrorProps?.opts
+    );
+    this.name = 'StrTooShort';
+  }
+}
+
+export class UrlNotValidException extends Error {
+  constructor ( customErrorProps?: CustomErrorProps ) {
+    const message = 'URL no válida';
+
+    super(
+      customErrorProps?.customMessage ?? message
+      // customErrorProps?.opts
+    );
+    this.name = 'UrlNotValid';
+  }
+}
