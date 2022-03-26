c5724b50a6b6164bbcd0a0dae3f6812e4fdc655c
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-03-25 22:43:02 -0300

    :triangular_flag_on_post: build: Build all basket use cases

--- Domain/Entities/Basket/UseCases/index.ts
+++ Domain/Entities/Basket/UseCases/index.ts
@@ -1,39 +1,92 @@
 import { Basket } from '@Domain/Entities/Basket/Basket';
-import { ProductParams } from '@Domain/Entities/Product/Product';
-import { BasketCommandRepository, BasketQueryRepository } from '@Domain/Entities/Basket/Respositories';
+import {
+  BasketCommandRepository,
+  BasketQueryRepository,
+} from '@Domain/Entities/Basket/Respositories';
+import { MethodNotExecutedException, NotFoundException } from '@Domain/Common/Exceptions';
+import { Product, ProductParams } from '@Domain/Entities/Product/Product';
+import { UUID, Uuid } from '@Domain/Common/Libraries';
 
-interface BasketUseCasesConstructor {
-  basket: Basket;
-  basketCommandRepository: BasketCommandRepository;
-  basketQueryRepository: BasketQueryRepository;
+export class CreateBaskteUseCase {
+  private _basket: Basket | null = null;
+
+  get basket() {
+    if ( this._basket === null ) throw new MethodNotExecutedException( 'Basket not created' );
+
+    return this._basket;
+  }
+
+  execute(): Basket {
+    if ( this._basket !== null ) return this._basket;
+    this._basket = Basket.create();
+    return this._basket;
+  }
 }
 
-export class BasketUseCases {
-  private _basket: Basket;
-
+export class SaveBasketUseCase {
   private _basketCommandRepository: BasketCommandRepository;
 
+  private _uuid: Uuid;
+
+  constructor( uuid: Uuid, basketCommandRepository: BasketCommandRepository ) {
+    this._basketCommandRepository = basketCommandRepository;
+    this._uuid = uuid;
+  }
+
+  execute( basket: Basket ): Promise<Basket> {
+    basket.id = this._uuid.generate();
+    return this._basketCommandRepository.saveBasket( basket );
+  }
+}
+
+export class GetBasketByIdUseCase {
   private _basketQueryRepository: BasketQueryRepository;
 
-  constructor( { basket, basketCommandRepository, basketQueryRepository }: BasketUseCasesConstructor ) {
-    this._basket = basket;
-    this._basketCommandRepository = basketCommandRepository;
+  constructor( basketQueryRepository: BasketQueryRepository ) {
     this._basketQueryRepository = basketQueryRepository;
   }
-  
-  addProductUseCase( product: ProductParams ): Basket {
-    return this._basketCommandRepository.saveProduct( product );
+
+  async execute( basketId: UUID ): Promise<Basket> {
+    const basket = await this._basketQueryRepository.getBasketById( basketId );
+
+    if ( basket === null ) throw new NotFoundException();
+
+    return basket;
+  }
+}
+
+export class AddProductUseCase {
+  private readonly _basket: Basket;
+
+  private _product: Product | null = null;
+
+  constructor( basket: Basket ) {
+    this._basket = basket;
   }
 
-  removeProductUseCase( productId: string ): Basket {
-    return this._basketCommandRepository.removeProduct( productId );
+  get latestsavedProduct(): Product {
+    if ( this._product === null ) throw new MethodNotExecutedException();
+
+    return this._product;
   }
 
-  saveBasketUseCase(): Promise<Basket> {
-    return this._basketCommandRepository.saveBasket( );
+  execute( product: ProductParams ): Basket {
+    this._product = Product.create( product );
+    this._basket.items.push( this._product );
+    return this._basket;
+  }
+}
+
+export class RemoveProductUseCase {
+  private readonly _basket: Basket;
+
+  constructor( basket: Basket ) {
+    this._basket = basket;
   }
 
-  getBasketUseCase( basketId: string ): Promise<Basket | null> {
-    return this._basketQueryRepository.getBasket( basketId );
+  execute( productId: UUID ): Basket {
+    const removeProduct = ( product: Product ) => product.id !== productId;
+    this._basket.items = this._basket.items.filter( removeProduct );
+    return this._basket;
   }
 }
