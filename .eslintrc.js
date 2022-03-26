273cef27a37561de49dd6368fc3b20935e20f25c
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-03-25 22:34:14 -0300

    :wrench: config: Refactor eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,57 @@
-const RULE = {
-  OFF: 'off',
-  WARN: 'warn',
+const VALUE_RULE = {
   ERROR: 'error',
+  WARN: 'warn',
+  OFF: 'off',
 };
+
+const RULES = {
+  TS: {
+    '@typescript-eslint/indent': [ VALUE_RULE.ERROR, 2 ],
+    '@typescript-eslint/no-explicit-any': VALUE_RULE.OFF,
+    '@typescript-eslint/no-unused-vars': [ VALUE_RULE.WARN, { varsIgnorePattern: '__' } ],
+  },
+  JEST: {
+    'jest/no-disabled-tests': VALUE_RULE.WARN,
+    'jest/no-focused-tests': VALUE_RULE.ERROR,
+    'jest/no-identical-title': VALUE_RULE.ERROR,
+    'jest/prefer-to-have-length': VALUE_RULE.WARN,
+    'jest/valid-expect': VALUE_RULE.ERROR,
+  },
+  REACT: {
+    'react/jsx-filename-extension': [ VALUE_RULE.ERROR, { extensions: [ '.jsx', '.tsx' ] } ],
+    'react/react-in-jsx-scope': VALUE_RULE.OFF,
+  },
+  VUE: {
+    'vue/multi-word-component-names': VALUE_RULE.OFF,
+  },
+  ESLINT: {
+    'array-bracket-spacing': [ VALUE_RULE.ERROR, 'always' ],
+    'arrow-parens': [ VALUE_RULE.ERROR, 'as-needed' ],
+    'computed-property-spacing': [ VALUE_RULE.ERROR, 'always' ],
+    'import/extensions': VALUE_RULE.OFF,
+    'import/no-unresolved': VALUE_RULE.OFF,
+    'import/prefer-default-export': VALUE_RULE.OFF,
+    'max-len': [ VALUE_RULE.ERROR, { code: 160, comments: 160 } ],
+    'max-classes-per-file': VALUE_RULE.OFF,
+    'no-console': [ VALUE_RULE.ERROR, { allow: [ 'error', 'warn', 'dir', 'info' ] } ],
+    'no-continue': VALUE_RULE.OFF,
+    'no-plusplus': VALUE_RULE.OFF,
+    'no-this-before-super': VALUE_RULE.OFF,
+    'no-param-reassign': VALUE_RULE.OFF,
+    'no-restricted-syntax': VALUE_RULE.OFF,
+    'no-underscore-dangle': VALUE_RULE.OFF,
+    'no-unused-expressions': [ VALUE_RULE.ERROR, { allowTernary: true } ],
+    'no-unused-vars': VALUE_RULE.ERROR,
+    'object-curly-spacing': [ VALUE_RULE.ERROR, 'always' ],
+    'semi': [ VALUE_RULE.ERROR, 'always' ],
+    'sort-imports': [ VALUE_RULE.ERROR, { allowSeparatedGroups: true, memberSyntaxSortOrder: [ 'none', 'all', 'single', 'multiple' ] } ],
+    'space-before-function-paren': [ VALUE_RULE.ERROR, 'never' ],
+    'space-in-parens': [ VALUE_RULE.ERROR, 'always' , { 'exceptions': [ 'empty' ] } ],
+    'template-curly-spacing': [ VALUE_RULE.ERROR, 'always' ],
+    'quotes': [ VALUE_RULE.ERROR, 'single' ],
+  },
+};
+
 
 module.exports = {
   root: true,
@@ -19,6 +68,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:jsdoc/recommended'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -30,39 +80,12 @@ module.exports = {
   },
   plugins: [
     'react',
-    'eslint-plugin-tsdoc'
+    'jsdoc',
   ],
   rules: {
-    '@typescript-eslint/indent': [ RULE.ERROR, 2 ],
-    '@typescript-eslint/no-explicit-any': RULE.OFF,
-    '@typescript-eslint/no-unused-vars': [ RULE.WARN, { varsIgnorePattern: '__' } ],
-    'array-bracket-spacing':  [ RULE.ERROR, 'always' ],
-    'arrow-parens':  [ RULE.ERROR, 'as-needed' ],
-    'computed-property-spacing':  [ RULE.ERROR, 'always' ],
-    'import/extensions':  RULE.OFF,
-    'import/no-unresolved':  RULE.OFF,
-    'import/prefer-default-export':  RULE.OFF,
-    'jest/no-disabled-tests':  RULE.WARN,
-    'jest/no-focused-tests':  RULE.ERROR,
-    'jest/no-identical-title':  RULE.ERROR,
-    'jest/prefer-to-have-length':  RULE.WARN,
-    'jest/valid-expect':  RULE.ERROR,
-    'max-len':  [ RULE.ERROR, { code: 160, comments: 160 } ],
-    'max-classes-per-file':  RULE.OFF,
-    'no-console':  [ RULE.ERROR, { allow: [ 'error', 'warn', 'dir', 'info' ] } ],
-    'no-plusplus':  RULE.OFF,
-    'no-this-before-super': RULE.OFF,
-    'no-underscore-dangle':  RULE.OFF,
-    'no-unused-vars':  RULE.ERROR,
-    'object-curly-spacing':  [ RULE.ERROR, 'always' ],
-    'react/jsx-filename-extension':  [ RULE.ERROR, { extensions: [ '.jsx', '.tsx' ] } ],
-    'react/react-in-jsx-scope':  RULE.OFF,
-    'require-jsdoc':  RULE.WARN,
-    'semi': [ RULE.ERROR, 'always' ],
-    'sort-imports':  [ RULE.ERROR, { allowSeparatedGroups: true, memberSyntaxSortOrder: [ 'none', 'all', 'single', 'multiple' ] } ],
-    'space-in-parens':  [ RULE.ERROR, 'always' ],
-    'template-curly-spacing':  [ RULE.ERROR, 'always' ],
-    'tsdoc/syntax': RULE.WARN,
-    'quotes': [ RULE.ERROR, 'single' ],
+    ...RULES.TS,
+    ...RULES.JEST,
+    ...RULES.REACT,
+    ...RULES.ESLINT,
   },
 };
