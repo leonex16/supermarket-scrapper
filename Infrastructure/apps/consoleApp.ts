cbe24c38947d7e5c119a2fadebb2a18e9b31ed28
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-03-25 22:45:07 -0300

    :sparkles: Console app is working!!!! (improve version)

--- Infrastructure/apps/consoleApp.ts
+++ Infrastructure/apps/consoleApp.ts
@@ -1,9 +1,10 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { BasketCommandRepositoryInMemory } from '@Infrastructure/Implementations/InMemory/BasketCommandRepositoryInMemory';
-import { BasketQueryRepositoryInMemory } from '@Infrastructure/Implementations/InMemory/BasketQueryRepositoryInMemory';
+import * as BasketUseCases from '@Domain/Entities/Basket/UseCases';
+import { BasketRepositoryImplementInMemory } from '@Infrastructure/Implementations/InMemory/BasketRepositoryImplementInMemory';
+import { UUIDImplementation } from '@Infrastructure/Libraries';
+import basketsRaw from '@Infrastructure/Common/Data/baskets.json';
 
 const newProduct = {
+  'id': '1',
   'name': 'product-name',
   'description': 'product-description',
   'detail': {
@@ -15,12 +16,27 @@ const newProduct = {
   'source': 'https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url'
 };
 
-export const consoleApp = async () => {
-  const basketCommandRepositoryInMemory = new BasketCommandRepositoryInMemory();
-  const basketQueryRepositoryInMemory = new BasketQueryRepositoryInMemory();
+const logger = ( title: string, value: any ) => {
+  const separator = '*'.repeat( 20 );
 
-  const basket = await basketQueryRepositoryInMemory.getBasket( 'basket-id' );
-  const basketWithItem = basketCommandRepositoryInMemory.saveProduct( newProduct );
-  const basketSaved = await basketCommandRepositoryInMemory.saveBasket( );
-  console.log( basket );
+  global.console.info( `\n${ separator } ${ title } ${ separator }\n` );
+  global.console.dir( value );
+  global.console.info( '\n' );
+};
+
+
+export const consoleApp = async() => {
+  const uuid             = new UUIDImplementation();
+  const basketRepository = new BasketRepositoryImplementInMemory( basketsRaw );
+  const createBaskte     = new BasketUseCases.CreateBaskteUseCase();
+  const addProduct       = new BasketUseCases.AddProductUseCase( createBaskte.execute() );
+  const removeproduct    = new BasketUseCases.RemoveProductUseCase( createBaskte.basket );
+  const savebasket       = new BasketUseCases.SaveBasketUseCase( uuid, basketRepository );
+  const getbasketbyid    = new BasketUseCases.GetBasketByIdUseCase( basketRepository );
+
+  logger( 'CREATE_BASKTE_USE_CASE', createBaskte );
+  logger( 'ADD_PRODUCT_USE_CASE', addProduct.execute( newProduct ) );
+  logger( 'REMOVE_PRODUCT_USE_CASE', removeproduct.execute( '1' ) );
+  logger( 'SAVE_BASKET_USE_CASE', await savebasket.execute( createBaskte.basket ) );
+  logger( 'GET_BASKET_BY_ID_USE_CASE', await getbasketbyid.execute( createBaskte.basket.id ) );
 }; 
