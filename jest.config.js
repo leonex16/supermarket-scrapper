f9286551c423ba30dd1451d54d734caf49c386e0
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-04-02 11:43:01 -0300

    :wrench: config: Create jest config file

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+const TS_CONFIG = {
+  APPLICATION: 'Application/tsconfig.json',
+  DOMAIN: 'Domain/tsconfig.json',
+  INFRASTRUCTURE: 'Infrastructure/tsconfig.json',
+};
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  rootDir: '.',
+  testRegex: '(spec.ts)',
+  verbose: false,
+  moduleNameMapper: {
+    '@Application/(.*)': '<rootDir>/Application/$1',
+    '@Domain/(.*)': '<rootDir>/Domain/$1',
+    '@Infrastructure/(.*)': '<rootDir>/Infrastructure/$1'
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: TS_CONFIG[ process.env.LAYERS_TEST_ENV ]
+    }
+  }
+};
