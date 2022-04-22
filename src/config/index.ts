b8b9af2ba2fc8193346e0f5151e8355c34b3a3ac
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-04-22 14:48:06 -0400

    Export SSL key and certificate as env vars

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,5 @@
 export const ENV = process.env.ENV ?? 'DEVELOPMENT';
 export const HOST = process.env.HOST ?? '127.0.0.1';
 export const PORT = process.env.PORT ?? '8080';
+export const SSL_CERT_PATH = process.env?.SSL_CERT_PATH;
+export const SSL_KEY_PATH = process.env?.SSL_KEY_PATH;
