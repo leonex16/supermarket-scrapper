fb588dd6624d96c867ef0bc644821d0feb2b344b
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-04-26 10:36:44 -0400

    Rename env  variable ENV to NODE_ENV

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-export const ENV = process.env.ENV ?? 'DEVELOPMENT';
+export const NODE_ENV = process.env.NODE_ENV ?? 'DEVELOPMENT';
 export const HOST = process.env.HOST ?? '127.0.0.1';
 export const PORT = process.env.PORT ?? '8080';
 export const SSL_CERT_PATH = process.env?.SSL_CERT_PATH;
