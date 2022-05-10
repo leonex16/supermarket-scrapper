d9f908add254884acb7a1c7583b30c8585d49282
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-05-10 10:36:39 -0400

    :wrench: config: Update jest config for server tests

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,10 @@ const TS_CONFIG = {
   DOMAIN: 'Domain/tsconfig.json',
   INFRASTRUCTURE: 'Infrastructure/tsconfig.json',
 };
+const pathToTsConfig = TS_CONFIG[ process.env.LAYERS_TEST_ENV ];
 
 module.exports = {
+  // moduleNameMapper: pathsToModuleNameMapper( compilerOptions.paths, { prefix: '<rootDir>' } ),
   preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: '.',
@@ -13,15 +15,16 @@ module.exports = {
   verbose: false,
   moduleNameMapper: {
     '@Application/(.*)': '<rootDir>/Application/$1',
+    '@server/(.*)': '<rootDir>/Application/pages/api/src/$1',
     '@Domain/(.*)': '<rootDir>/Domain/$1',
-    '@Infrastructure/(.*)': '<rootDir>/Infrastructure/$1'
+    '@Infrastructure/(.*)': '<rootDir>/Infrastructure/$1',
   },
   globals: {
     'ts-jest': {
-      tsconfig: TS_CONFIG[ process.env.LAYERS_TEST_ENV ],
+      tsconfig: pathToTsConfig,
       diagnostics: {
         exclude: [ '**' ],
       },
-    }
-  }
+    },
+  },
 };
