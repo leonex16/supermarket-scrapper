7755b8a06c5bafee1eb545a9f0997703f4666b5b
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-05-13 16:57:53 -0400

    :memo: docs: Add linebar docs

---
+++ Application/src/__stories__/components/molecules/LineBar.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { LineBar } from '@Application/src/components/molecules/LineBar';
+
+export function LineBars() {
+  return (
+    <>
+      <section>
+        <LineBar />
+      </section>
+      <hr />
+    </>
+  );
+}
