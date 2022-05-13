62f7dbb3b479bf11eb06248b550443be80fa935d
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-05-13 16:17:35 -0400

    :test_tube: test: Add headline tests

--- Application/src/__tests__/components/molecules/Button.spec.tsx
+++ Application/src/__tests__/components/molecules/Button.spec.tsx
@@ -1,20 +1,27 @@
 
 import React from 'react'
+import { render, fireEvent, screen } from '@testing-library/react';
+
 import { Button, ButtonColor, ButtonProps } from '@Application/src/components/molecules/Button';
-import { render, fireEvent } from '@testing-library/react';
+
+const props: ButtonProps = {
+  color: ButtonColor.Primary,
+  onClick: jest.fn(),
+  text: 'Button Text'
+};
+
+beforeEach( async () => {
+  render(<Button {...props} />)
+})
 
 describe('<Button />', () => {
   test('Render component', () => {
-    const props: ButtonProps = {
-      color: ButtonColor.Primary,
-      onClick: jest.fn(),
-      text: 'Button Text'
-    };
-    
-    const component = render(<Button {...props} />)
+    screen.getByText(props.text)
+  })
 
-    fireEvent.click(component.getByText(props.text))
+  test('Check fire onClick function', () => {
+    fireEvent.click(screen.getByText(props.text))
 
-    expect(props.onClick).toHaveBeenCalledTimes(1)
+    expect(props.onClick).toBeCalledTimes(1)
   })
 })
