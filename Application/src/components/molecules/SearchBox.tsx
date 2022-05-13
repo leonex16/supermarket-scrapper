cb5a9025213edd7f646ddae692c73d672481413f
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-05-13 19:46:32 -0400

    :triangular_flag_on_post: build: Add searchbox component

---
+++ Application/src/components/molecules/SearchBox.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Image from 'next/image';
+
+import styles from '@Application/styles/molecules/SearchBox.module.scss'
+
+export interface SearchBoxProps {
+  handleSubmit: React.FormEventHandler<HTMLFormElement>,
+  placeholder: string,
+}
+
+export function SearchBox({handleSubmit, placeholder}: SearchBoxProps) {
+  return (
+    <form className={`${styles['scr-search-box']}`} onSubmit={handleSubmit} role={'searchbox'} >
+      <input className={`${styles['scr-search-box__input']}`} placeholder={placeholder} name="search-input" type="text" />
+      <button className={`${styles['scr-search-box__btn']}`}>
+        <Image className={`${styles['scr-search-box__ico']}`} src="/icons/search.svg" alt="search button" role={'search'} width={24} height={24}  />
+      </button>
+    </form>
+  )
+}
