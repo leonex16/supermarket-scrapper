b3d42abe83db6989379006399d52b945b72494e2
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-05-14 19:36:37 -0400

    :triangular_flag_on_post: build: Add chipsfilter component

--- Application/src/components/molecules/SearchBox.tsx
+++ Application/src/components/molecules/SearchBox.tsx
@@ -13,7 +13,7 @@ export function SearchBox({handleSubmit, placeholder}: SearchBoxProps) {
     <form className={`${styles['scr-search-box']}`} onSubmit={handleSubmit} role={'searchbox'} >
       <input className={`${styles['scr-search-box__input']}`} placeholder={placeholder} name="search-input" type="text" />
       <button className={`${styles['scr-search-box__btn']}`}>
-        <Image className={`${styles['scr-search-box__ico']}`} src="/icons/search.svg" alt="search button" role={'search'} width={24} height={24}  />
+        <Image src="/icons/search.svg" alt="search button" role={'search'} width={24} height={24}  />
       </button>
     </form>
   )
