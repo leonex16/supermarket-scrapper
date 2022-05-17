ada0ad82aa2fea3645242d739442203392daeedc
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-05-17 19:41:32 -0400

    :memo: docs: Add productCard docs

---
+++ Application/src/__stories__/components/organisms/ProductCard.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ProductCard as Component, ProductCardProps } from '@Application/src/components/organisms/ProductCard';
+
+export function ProductCard() {
+  const props: ProductCardProps = {
+    name: 'Product Name',
+    description: 'Product Description',
+    unit: 'Kg',
+    price: '$100',
+    image: 'https://picsum.photos/200/300',
+    source: 'https://dummyimage.com/400x600/000/fff&text=ProductImage',
+  };
+
+  return (
+    <>
+      <section>
+        <Component {...props} />
+      </section>
+      <hr />
+    </>
+  );
+}
