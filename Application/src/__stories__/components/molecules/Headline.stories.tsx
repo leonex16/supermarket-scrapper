ad745d460152ef5852ea69ba545bd0493aae11e1
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-05-23 08:54:06 -0400

    :memo: docs: Update Headline story

--- Application/src/__stories__/components/molecules/Headline.stories.tsx
+++ Application/src/__stories__/components/molecules/Headline.stories.tsx
@@ -10,32 +10,32 @@ export function Headlines() {
   const headlines = [
     {
       supermarket: HeadlineSupermarket.Jumbo,
-      text: 'Headline Text',
+      text: 'Headline Text H1',
       type: HeadlineType.H1,
     },
     {
       supermarket: HeadlineSupermarket.Lider,
-      text: 'Headline Text',
+      text: 'Headline Text H2',
       type: HeadlineType.H2,
     },
     {
       supermarket: HeadlineSupermarket.SantaIsabel,
-      text: 'Headline Text',
+      text: 'Headline Text H3',
       type: HeadlineType.H3,
     },
     {
       supermarket: HeadlineSupermarket.Tottus,
-      text: 'Headline Text',
+      text: 'Headline Text H4',
       type: HeadlineType.H4,
     },
     {
       supermarket: HeadlineSupermarket.Jumbo,
-      text: 'Headline Text',
+      text: 'Headline Text H5',
       type: HeadlineType.H5,
     },
     {
       supermarket: HeadlineSupermarket.Lider,
-      text: 'Headline Text',
+      text: 'Headline Text H6',
       type: HeadlineType.H6,
     },
   ];
